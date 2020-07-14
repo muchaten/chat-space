5aa3702a7f773bd8f35a8f8fc5dbe6ceebe5a1fb
muchaten <muchaten4567@gmail.com>
2020-07-14 16:48:21 +0900

    message_form_ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,68 @@
+$(function(){
+
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageBox">
+          <div class="message__top">
+            <div class="member__name">
+              ${message.user_name}
+            </div>
+            <div class="message__day">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox">
+        <div class="message__top">
+          <div class="member__name">
+            ${message.user_name}
+          </div>
+          <div class="message__day">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.Main_chat__message').append(html);
+      $('.Main_chat__message').animate({ scrollTop: $('.Main_chat__message')[0].scrollHeight});
+      $('form_').val('');
+      $('.Form__editbtn').prop('disabled', false);
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    })
+  })
+});
